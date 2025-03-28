3077107efe8b82c0bb8823aac37e72df620ec605
kaiqui coelho <kkaiqui.12@gmail.com>
2025-03-27 23:48:54 -0300

    Modificações na home, about

--- src/components/Sections/About/About.tsx
+++ src/components/Sections/About/About.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
 import styles from './About.module.css';
 
-const About: React.FC = () => {
-  return (
-    <section id="about" className={styles.aboutSection}>
-      <div className={styles.presentation}>
-          <h1>Olá, eu sou [Seu Nome]</h1>
-          <p>Desenvolvedor Full Stack</p>
-          <div className={styles.description}>
-            <p>Sou apaixonado por criar soluções tecnológicas inovadoras</p>
-            <p>Especialista em React, Node.js e desenvolvimento de sistemas escaláveis</p>
-            <p>Especialista em React, Node.js e desenvolvimento de sistemas escaláveis</p>
-            <p>Especialista em React, Node.js e desenvolvimento de sistemas escaláveis</p>
-            <p>Especialista em React, Node.js e desenvolvimento de sistemas escaláveis</p>
-            <p>Especialista em React, Node.js e desenvolvimento de sistemas escaláveis</p>
-            <p>Especialista em React, Node.js e desenvolvimento de sistemas escaláveis</p>
-            <p>Especialista em React, Node.js e desenvolvimento de sistemas escaláveis</p>
-            <p>+5 anos de experiência em projetos complexos</p>
+export default function About() {
+  const videoId = "SEU_VIDEO_ID" // Substitua pelo ID do seu vídeo
+    return (
+      <section className={styles.about}>
+        <div className={styles.container}>
+          <div className={styles.videoWrapper}>
+            <iframe
+              src={`https://www.youtube.com/embed/${videoId}?autoplay=1&loop=1&playlist=${videoId}&controls=0&mute=1`}
+              title="Apresentação"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              className={styles.video}
+            ></iframe>
+          </div>
+          
+          <div className={styles.presentation}>
+            <h1>Olá, eu sou [Seu Nome]</h1>
+            <p>Desenvolvedor Full Stack</p>
+            <div className={styles.description}>
+              <p>Sou apaixonado por criar soluções tecnológicas inovadoras</p>
+              <p>Especialista em React, Node.js e desenvolvimento de sistemas escaláveis</p>
+              <p>+5 anos de experiência em projetos complexos</p>
+              <p>+5 anos de experiência em projetos complexos</p>
+              <p>+5 anos de experiência em projetos complexos</p>
+              <p>+5 anos de experiência em projetos complexos</p>
+              <p>+5 anos de experiência em projetos complexos</p>
+              <p>+5 anos de experiência em projetos complexos</p>
+              <p>+5 anos de experiência em projetos complexos</p>
+              <p>+5 anos de experiência em projetos complexos</p>
+              <p>+5 anos de experiência em projetos complexos</p>
+              <p>+5 anos de experiência em projetos complexos</p>
+              <p>+5 anos de experiência em projetos complexos</p>
+            </div>
           </div>
         </div>
-    </section>
-  );
-};
+      </section>
+    )
 
-export default About;
+}
--- src/components/Sections/Home/Home.tsx
+++ src/components/Sections/Home/Home.tsx
@@ -1,21 +1,18 @@
 import styles from './Home.module.css'
+import userImage from './user.svg'; // Importação correta da imagem
 
 export default function Home() {
-  const videoId = "SEU_VIDEO_ID" // Substitua pelo ID do seu vídeo
-
   return (
     <section className={styles.home}>
       <div className={styles.container}>
-        <div className={styles.videoWrapper}>
-          <iframe
-            src={`https://www.youtube.com/embed/${videoId}?autoplay=1&loop=1&playlist=${videoId}&controls=0&mute=1`}
-            title="Apresentação"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            className={styles.video}
-          ></iframe>
+        {/* Container da imagem */}
+        <div className={styles.imageWrapper}>
+          <img 
+            src={userImage} // Caminho para sua imagem
+            alt="User"
+            className={styles.profileImage}
+          />
         </div>
-        
         <div className={styles.presentation}>
           <h1>Olá, eu sou [Seu Nome]</h1>
           <p>Desenvolvedor Full Stack</p>
